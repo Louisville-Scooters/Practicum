f08c12d34609acb4afd0c3d63c18896d18495aab
Eugene Chong <eugene.chong@gmail.com>
2020-02-01 00:55:21 -0500

    DC code

--- ~code/DC/DC - 01 - Read Scooter Data (Closed).R
+++ ~code/DC/DC - 01 - Read Scooter Data (Closed).R
@@ -1,3 +1,10 @@
+##########################################################################
+# This script is for reading in the DC scooter data.
+# This makes no changes to the data except:
+# 1. change all the column names to all lower case, as it was inconsistent
+# 2. add a column 'dataset' that notes the csv file the data came from
+##########################################################################
+
 # Set directory for DC data
 DC_directory <- paste(data_directory, 
                         "/DC/",
@@ -9,10 +16,9 @@ DC_scooter_trip_list <- list.files(path = DC_directory,
 
 ## Check that this covers all scooter files
 # DC_scooter_trip_list_b <- list.files(path = DC_directory) # all files in the folder
-
 # setdiff(DC_scooter_trip_list_b, DC_scooter_trip_list) # difference between all files and just scooter files
 
-DC_scooter_data <- DC_scooter_trip_list[1:10] %>%
+DC_scooter_data_raw <- DC_scooter_trip_list %>%
   {.[!str_detect(., "2019-09_Lime_trips.csv")]} %>%  # this particular record is tab-separated rather than comma-separated
   {paste(DC_directory, ., sep = "")} %>% 
   map_dfr(., 
@@ -28,10 +34,3 @@ DC_scooter_data <- DC_scooter_trip_list[1:10] %>%
               read_tsv(col_types = cols(.default = "c")) %>% 
               set_names(., tolower(names(.))) %>% 
               mutate(dataset = "2019-09_Lime_trips"))
-
-test <- DC_scooter_data%>% 
-  mutate(company = tolower(str_extract(dataset, "Lime|Bird|JUMP|skip|Spin|Razor|razor|Lyft")),
-         start_date = str_extract(start_time, "[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{1,2}"))
-  
-
-
---
+++ ~code/DC/DC - 02 - Clean Data (Open).R
@@ -0,0 +1,63 @@
+##########################################################################
+# This script is for cleaning the DC scooter data
+# It:
+# 1. Names a projection to use for DC
+# 2. Adds some helper columns: company, start and end dates and times
+# 3. Defines a function for creating DC scooter origin and destination sf objects, which can be linked via the 'trip_id' column to the rest of the dataset
+#    The DC scooter data should be filtered to fewer rows before running this function, as the objects would be massive
+##########################################################################
+
+# DC projection
+DC_proj <- 2283 # Northern Virginia: https://epsg.io/2283
+
+# Add helper columns
+DC_scooter_data <- DC_scooter_data_raw %>% 
+  mutate(# this is the start time from the original data. Some data includes the date, but others only have the time
+    original_start_time = start_time,
+    company = tolower(str_extract(dataset, "Lime|Bird|JUMP|skip|Spin|Razor|razor|Lyft")),         
+    start_date = str_extract(original_start_time, "[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{1,2}"),
+    start_time = str_extract(original_start_time, "[0-9]{1,2}:[0-9]{1,2}"),
+    original_end_time = end_time,
+    company = tolower(str_extract(dataset, "Lime|Bird|JUMP|skip|Spin|Razor|razor|Lyft")),         
+    end_date = str_extract(original_end_time, "[0-9]{1,2}\\/[0-9]{1,2}\\/[0-9]{1,2}"),
+    end_time = str_extract(original_end_time, "[0-9]{1,2}:[0-9]{1,2}"))
+
+# Make sf objects
+make_DC_sf <- function(x, # x should be 'DC_scooter_data'
+                       trip_end, # define whether you want the origins or the destinations
+                       proj) { # proj should be 'DC_proj'
+  
+  if(!grepl("ori|des", trip_end)) {
+    
+    stop("trip_end must be either 'origins' or 'dests'")
+    
+  } else if (grepl("ori", trip_end)) {
+    
+    output <- x %>%
+      dplyr::select(trip_id,
+                    company,
+                    start_lat,   
+                    start_lon) %>% 
+      st_as_sf(coords = c("start_lon", "start_lat"), 
+               crs = 4326) %>% 
+      st_transform(proj)
+    
+    } else {
+    
+    output <- x %>%
+      dplyr::select(trip_id,
+                    company,
+                    end_lat,   
+                    end_lon) %>% 
+      st_as_sf(coords = c("end_lon", "end_lat"), 
+               crs = 4326) %>% 
+      st_transform(proj)
+    
+    }
+  output
+}
+
+# Example of make_DC_sf() function
+# DC_origins_sf <- make_DC_sf(DC_scooter_data[1:10,], 
+#                             trip_end = "origins", 
+#                             proj = DC_proj)
