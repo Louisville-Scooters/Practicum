c10b4b5a7f8330effeca8b553953c8ab61494f43
Ophelia Liu <ophelialiuyj@gmail.com>
2020-02-02 16:44:14 -0500

    add MNP-02

---
+++ ~code/MNP/MNP - 02 - Clean Data (Open).R
@@ -0,0 +1,8 @@
+##########################################################################
+# This script is for cleaning the MNP scooter data
+# It:
+# 1. Adds some helper columns: start and end dates and times
+# 2. Links geographical information to the data based on streetcenterlineID
+##########################################################################
+
+# Add helper columns ----
