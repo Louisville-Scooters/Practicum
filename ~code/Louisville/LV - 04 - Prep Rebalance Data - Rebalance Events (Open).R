b63bbc703b165238233d87d9fed57f9bfb377632
Ophelia Liu <ophelialiuyj@gmail.com>
2020-02-02 15:07:40 -0500

    update on LV-04-Prep Rebalance Data

--- ~code/Louisville/LV - 04 - Prep Rebalance Data - Rebalance Events (Open).R
+++ ~code/Louisville/LV - 04 - Prep Rebalance Data - Rebalance Events (Open).R
@@ -17,5 +17,40 @@ LV_rebal_rebalance_only <- LV_rebal_sf %>%
 
 # June 2019 ----
 LV_rebal_rebalance_only_0619 <- LV_rebal_rebalance_only %>%
-  filter(year(occurredAt) == 2019, month(occurredAt) == 6) 
+  filter(year(occurredAt) == 2019, month(occurredAt) == 6)
+
+# Trim the dataset to be "pick-up; drop-off" format b/c sometimes there is [reb pick up, reb pick up, reb drop off]
+LV_reb_ID_list <- c()
+for (veh in unique(LV_rebal_rebalance_only_0619$vehicleId)) {
+  this_vehicle_set <- LV_rebal_rebalance_only_0619 %>% filter(vehicleId == veh)
+  print(veh)
+  output_list <- c() # initialize list
+  for (i in 1:nrow(this_vehicle_set)) {
+      if (this_vehicle_set$reason[i] == 'rebalance drop off') {
+        output_list <- append(output_list, c(this_vehicle_set$id[i], this_vehicle_set$id[i-1])) #store the id of rebalance drop off and rebalance pick up before it
+  }}
+    LV_reb_ID_list <- append(LV_reb_ID_list, output_list) #this should store all the [pick-up, drop-off] pairs
+  } 
+
+
+LV_rebal_rebalance_only_0619_trim1 <- LV_rebal_rebalance_only_0619 %>%
+  filter(LV_rebal_rebalance_only_0619$id %in% LV_reb_ID_list) # Trim the data set
+
+# Notice the dataset has some weird [pick-up, drop-off, drop-off] and some of them does not have [pick-up] info at all
+# We trim the dataset one more time by the other way round
+LV_reb_ID_list2 <- c()
+for (veh in unique(LV_rebal_rebalance_only_0619_trim1$vehicleId)) {
+  this_vehicle_set <- LV_rebal_rebalance_only_0619_trim1 %>% filter(vehicleId == veh)
+  print(veh)
+  output_list <- c() # initialize list
+  for (i in 1:nrow(this_vehicle_set)) {
+    if (this_vehicle_set$reason[i] == 'rebalance pick up') {
+      output_list <- append(output_list, c(this_vehicle_set$id[i], this_vehicle_set$id[i+1])) #store the id of rebalance drop off and rebalance pick up before it
+    }}
+  LV_reb_ID_list2 <- append(LV_reb_ID_list2, output_list) #this should store all the [pick-up, drop-off] pairs
+} 
+
+# This should be our final dataset to use to generate trip origin-destination table
+LV_rebal_rebalance_only_0619_trim2 <- LV_rebal_rebalance_only_0619_trim1 %>%
+  filter(LV_rebal_rebalance_only_0619_trim1$id %in% LV_reb_ID_list2) 
 
