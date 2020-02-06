c5752075e6e08f18a96a7143bcad0f49341c70c4
XinyiMsumyee <hisumyee@hotmail.com>
2020-02-06 10:36:30 -0500

    code for collecting census data for AU & KC

--- ~code/Kansas City/KC - 21 - Collect and Clean Census Data (Closed).R
+++ ~code/Kansas City/KC - 21 - Collect and Clean Census Data (Closed).R
@@ -13,29 +13,44 @@ census_key <- readRDS(census_key_RDS)
 tidycensus::census_api_key(census_key, install = TRUE, overwrite = TRUE)
 
 # Collect census data and geometries
-AU_Census_raw <- get_acs(geography = "tract", 
+KC_Census_raw1 <- get_acs(geography = "tract", 
                          variables = census_vars, 
-                         year = 2017, 
-                         state = "TX", 
+                         year = 2018, 
+                         state = "MO", 
                          geometry = TRUE, 
-                         county=c("Travis"),
+                         county=c("Jackson"),
                          output = "wide") %>%
   rename_census_cols %>%
   dplyr::select(GEOID, 
                 geometry,
                 census_colNames) %>% 
-  st_transform(AU_proj)
+  st_transform(KC_proj)
 
-AU_Census_geoinfo <- AU_Census_raw %>%
+KC_Census_raw2 <- get_acs(geography = "tract", 
+                         variables = census_vars, 
+                         year = 2018, 
+                         state = "MO", 
+                         geometry = TRUE, 
+                         county=c("Johnson"),
+                         output = "wide") %>%
+  rename_census_cols %>%
+  dplyr::select(GEOID, 
+                geometry,
+                census_colNames) %>% 
+  st_transform(KC_proj)
+
+KC_Census_raw <- rbind(KC_Census_raw1, KC_Census_raw2)
+
+KC_Census_geoinfo <- KC_Census_raw %>%
   dplyr::select(GEOID, geometry)
-  # st_intersection(LV_SA %>% dplyr::select(geometry))
+# st_intersection(LV_SA %>% dplyr::select(geometry))
 
 # extract centroid of each census tract
-AU_Census_geoinfo <- AU_Census_geoinfo %>% 
-  mutate(centroid_X = st_coordinates(st_centroid(AU_Census_geoinfo))[, 1],
-         centroid_Y = st_coordinates(st_centroid(AU_Census_geoinfo))[, 2])
+KC_Census_geoinfo <- KC_Census_geoinfo %>% 
+  mutate(centroid_X = st_coordinates(st_centroid(KC_Census_geoinfo))[, 1],
+         centroid_Y = st_coordinates(st_centroid(KC_Census_geoinfo))[, 2])
 
-AU_Census <- AU_Census_raw %>% 
+KC_Census <- KC_Census_raw %>% 
   mutate(pWhite = White_Pop / TotPop,
          Mean_Commute_Time = Total_Travel_Time / Num_Commuters,
          pTrans = Total_Public_Trans / Means_of_Transport_pop,
@@ -46,8 +61,9 @@ AU_Census <- AU_Census_raw %>%
          pOccupied = Occupied/Total_occupancy,
          pVehAvai = 1 - No_vehicle / Vehicle_own_pop)
 
-# names(AU_Census)
+# names(KU_Census)
 
-AU_Census <- AU_Census %>%
+KC_Census <- KC_Census %>%
   dplyr::select(GEOID, TotPop, TotHseUni, MdHHInc, MdAge, MedValue, MedRent, pWhite, Mean_Commute_Time,
                 pTrans, pDrive, pFemale, pCom30plus, pOccupied, pVehAvai)
+
