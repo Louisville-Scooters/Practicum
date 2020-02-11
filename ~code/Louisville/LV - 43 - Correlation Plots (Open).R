26a3c9d348ccf4a681eb6d8fd36ccefeb720b3d5
Ophelia Liu <ophelialiuyj@gmail.com>
2020-02-10 22:13:17 -0500

    add correlation scatter plot of census var VS count of origin

--- ~code/Louisville/LV - 43 - Correlation Plots (Open).R
+++ ~code/Louisville/LV - 43 - Correlation Plots (Open).R
@@ -13,3 +13,22 @@ LV_corr_mat <- cor(LV_open_ct %>%
 LV_open_ct_corrplot <- corrplot(LV_corr_mat,
                                 method = "color")
 
+# Ophelia's Feb 10 update
+glimpse(LV_open_ct)
+LV_ori_correlation.long <-
+  st_set_geometry(LV_open_ct, NULL) %>%
+  dplyr::select(-GEOID, -centroid_X, -centroid_Y, -dests_cnt, -Mean_Commute_Time) %>%
+  gather(Variable, Value, -origins_cnt )
+
+LV_ori_correlation.cor <-
+  LV_ori_correlation.long %>%
+  group_by(Variable) %>%
+  summarize(correlation = cor(Value, origins_cnt, use = "complete.obs"))
+
+ggplot(LV_ori_correlation.long, aes(Value, origins_cnt)) +
+  geom_point(size = 0.1) +
+  geom_text(data = LV_ori_correlation.cor, aes(label = paste("r =", round(correlation, 2))),
+            x=-Inf, y=Inf, vjust = 1, hjust = -.1) +
+  geom_smooth(method = "lm", se = FALSE, colour = "gold") +
+  facet_wrap(~Variable, ncol = 4, scales = "free") +
+  labs(title = "Origin count as a function of census factors")
