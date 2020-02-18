d8ce83296b00127ce31b88fb6713c6821bc2c588
Ophelia Liu <ophelialiuyj@gmail.com>
2020-02-18 00:59:42 -0500

    added Model - 01 to set up datafiles for ML model building

---
+++ ~code/Model/Model - 01 - Setting up (Open).R
@@ -0,0 +1,41 @@
+##########################################################################
+# This script is for setting up data files for ML model building. 
+# It reads in:
+# 1. LV_spatial_census (has 0619 trip origin count by tract + census var + spatial var)
+# 2. MNP_
+# 3. CH_
+# 4. KS_
+# 5. 
+
+# This script exports the following data:
+# 1. 
+##########################################################################
+
+# Reads in data 
+LV_spatial_census_RDS <- file.path(data_directory, "~RData/Louisville/LV_spatial_census")
+LV_spatial_census <- readRDS(LV_spatial_census_RDS)
+
+
+# Concat data from all cities together
+ML_model_panel <- rbind(LV_spatial_census)
+
+glimpse(ML_model_panel)
+
+# Try linear regression model
+
+reg1 <- 
+  lm(origins_cnt ~  count_college + count_leisure + count_office + count_pubtran
+     + count_restaurant + count_retail + count_tourism + TotPop + TotHseUni
+     + MdHHInc + MdAge + MedValue + MedRent + pWhite + pTrans + pDrive + pFemale
+     + pCom30plus + pVehAvai, data= as.data.frame(ML_model_panel))
+
+summary(reg1)
+
+# Split train/test set
+inTrain <- createDataPartition (
+  y = paste(ML_model_panel$TotHseUni), 
+  p = .60, list = FALSE)
+ML.train <- ML_model_panel[inTrain,] 
+ML.test <- ML_model_panel[-inTrain,]  
+
+
