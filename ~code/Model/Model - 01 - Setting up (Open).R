f785a8c9bc6c2e9a234f3ae71f6b229127ef242f
Ophelia Liu <ophelialiuyj@gmail.com>
2020-02-25 21:07:25 -0500

    update

--- ~code/Model/Model - 01 - Setting up (Open).R
+++ ~code/Model/Model - 01 - Setting up (Open).R
@@ -14,12 +14,15 @@
 # Reads in data 
 LV_spatial_census_RDS <- file.path(data_directory, "~RData/Louisville/LV_spatial_census")
 LV_spatial_census <- readRDS(LV_spatial_census_RDS)
+MNP_spatial_census_RDS <- file.path(data_directory, "~RData/Louisville/MNP_spatial_census")
+MNP_spatial_census <- readRDS(MNP_spatial_census_RDS)
 
+#MNP does not have dest_cnt
+LV_spatial_census <- LV_spatial_census %>%
+  dplyr::select(-dests_cnt)
 
 # Concat data from all cities together
-ML_model_panel <- rbind(LV_spatial_census)
-
-glimpse(ML_model_panel)
+ML_model_panel <- rbind(LV_spatial_census%>% st_set_geometry(NULL), MNP_spatial_census%>% st_set_geometry(NULL))
 
 # Try linear regression model
 
@@ -27,7 +30,7 @@ reg1 <-
   lm(origins_cnt ~  count_college + count_leisure + count_office + count_pubtran
      + count_restaurant + count_retail + count_tourism + TotPop + TotHseUni
      + MdHHInc + MdAge + MedValue + MedRent + pWhite + pTrans + pDrive + pFemale
-     + pCom30plus + pVehAvai, data= as.data.frame(ML_model_panel))
+     + pCom30plus + pVehAvai + city, data= as.data.frame(ML_model_panel))
 
 summary(reg1)
 
