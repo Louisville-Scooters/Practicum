9e81e17a688b242103ec3df00388f44a2e9999a7
Eugene Chong <eugene.chong@gmail.com>
2020-04-01 22:02:26 -0400

    comment out saveRDS commands

--- ~code/Model/Model - 01 - Setting up (Open).R
+++ ~code/Model/Model - 01 - Setting up (Open).R
@@ -18,8 +18,8 @@ library(xgboost)
 
 # Reads in data 
 Model_panel_RDS <- file.path(data_directory, "~RData/Model_panel")
-saveRDS(Model_panel,
-        file = Model_panel_RDS)
+# saveRDS(Model_panel,
+#         file = Model_panel_RDS)
 Model_panel <- readRDS(Model_panel_RDS)
 
 Model_clean <- na.omit(Model_panel)
@@ -27,8 +27,8 @@ Model_clean <- Model_clean %>%
   dplyr::select(-c(MEAN_COMMUTE_TIME,GEOID, CENTROID_X, CENTROID_Y, CITY))
 
 Model_clean_RDS <- file.path(data_directory, "~RData/Model_clean")
-saveRDS(Model_clean,
-        file = Model_clean_RDS)
+# saveRDS(Model_clean,
+#         file = Model_clean_RDS)
 Model_clean <- readRDS(Model_clean_RDS)
 
 ### XINYI'S CODE ####
