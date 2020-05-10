95627693d9dfb477e76f8774ebbe0d9b393d29b9
xinyimsumyee <hisumyee@hotmail.com>
2020-05-10 17:30:26 -0400

    PLOT FOR LV

--- ~code/Louisville/LV - 23 - Aggregate Rides by Census Tract (Open).R
+++ ~code/Louisville/LV - 23 - Aggregate Rides by Census Tract (Open).R
@@ -7,21 +7,91 @@
 ### Open Data ----
 # Count origins for each census tract
 
+LV_open_origins_RDS <- file.path(data_directory,
+                                 "~RData/Louisville/LV_open_origins")
+
+LV_open_origins <- readRDS(LV_open_origins_RDS)
+
+
 LV_open_07to09_sf <- LV_open_origins %>%
-  filter(StartDate >= '2019-07-01' & StartDate < '2019-10-01')
+  filter(month(StartDate) >= '2019-07-01' & StartDate < '2019-10-01')
 
 LV_open_origins_ct <- LV_Census_ct %>% 
-  mutate(origins_cnt = (lengths(st_intersects(., LV_open_07to09_sf)))/3)
+  mutate(origins_cnt = (lengths(st_intersects(., LV_open_07to09_sf))))
 
 # Count dests for each census tract
-#LV_open_dests_ct <- LV_Census_ct %>% 
-#  mutate(dests_cnt = lengths(st_intersects(., LV_open_dests)))
+LV_open_dests_ct <- LV_Census_ct %>%
+ mutate(dests_cnt = lengths(st_intersects(., LV_open_dests)))
 
 # Combine
-# LV_open_ct <- LV_open_origins_ct %>% 
-#   left_join(LV_open_dests_ct %>% 
-#               st_drop_geometry() %>%
-#               dplyr::select(GEOID, dests_cnt),
-#             by = "GEOID")
+LV_open_ct <- LV_open_origins_ct %>%
+  left_join(LV_open_dests_ct %>%
+              st_drop_geometry() %>%
+              dplyr::select(GEOID, dests_cnt),
+            by = "GEOID")
 
 LV_open_ct <- LV_open_origins_ct
+
+
+LV_ORIGINS <- LV_scooter_ct %>%
+  group_by(Start.Census.Tract) %>% 
+  summarise(Outflow = n()) %>%
+  na.omit()
+
+LV_DESTS <- LV_scooter_ct %>%
+  group_by(End.Census.Tract) %>% 
+  summarise(Inflow = n()) %>%
+  na.omit()
+
+LV_net_inoutflow <- LV_open_ct %>% 
+  dplyr::select(GEOID, origins_cnt, dests_cnt) %>%
+  rename(Inflow = dests_cnt, Outflow = origins_cnt) %>%
+  mutate(NetInflow = Inflow - Outflow,
+         NetInflowRate = (Inflow - Outflow)/Inflow)
+LV_net_inoutflow[is.na(LV_net_inoutflow)] <- 0
+most_pickups <- LV_net_inoutflow$GEOID[LV_net_inoutflow$Outflow==max(LV_net_inoutflow$Outflow)]
+most_dropoffs <- LV_net_inoutflow$GEOID[LV_net_inoutflow$Inflow==max(LV_net_inoutflow$Inflow)]
+
+
+most_pickups_ct <- subset(LV_net_inoutflow,LV_net_inoutflow$GEOID==most_pickups)
+most_dropoffs_ct <- subset(LV_net_inoutflow,LV_net_inoutflow$GEOID==most_dropoffs)
+max_inflow <- max(abs(LV_net_inoutflow$NetInflow))
+max_inflowRate <- max(abs(LV_net_inoutflow$NetInflowRate))
+library(viridis)
+
+ggplot()+
+  geom_sf(data = st_sf(LV_net_inoutflow), aes(fill=Outflow)) +
+  #geom_sf(data = st_sf(most_pickups_ct), color='white', fill='transparent', size=1.2) +
+  #scale_fill_continuous(limits=c(-max_inflow, max_inflow)) +
+  scale_fill_viridis()+
+  #geom_sf(data = st_sf(most_dropoffs_ct), color='darkblue', fill='transparent', size=1.2) +
+  labs(title='Outflow Map for Louisville, KY') +
+  mapTheme()
+
+ggplot()+
+  geom_sf(data = st_sf(LV_net_inoutflow), aes(fill=Inflow)) +
+  #geom_sf(data = st_sf(most_pickups_ct), color='white', fill='transparent', size=1.2) +
+  #scale_fill_continuous(limits=c(-max_inflow, max_inflow)) +
+  scale_fill_viridis()+
+  #geom_sf(data = st_sf(most_dropoffs_ct), color='darkblue', fill='transparent', size=1.2) +
+  labs(title='Inflow Map for Louisville, KY') +
+  mapTheme()
+
+ggplot()+
+  geom_sf(data = st_sf(LV_net_inoutflow), aes(fill=NetInflow)) +
+  geom_sf(data = st_sf(most_pickups_ct), color='white', fill='transparent', size=1.2) +
+  #scale_fill_continuous(limits=c(-max_inflow, max_inflow)) +
+  scale_fill_viridis(limits=c(-max_inflow, max_inflow))+
+  #geom_sf(data = st_sf(most_dropoffs_ct), color='darkblue', fill='transparent', size=1.2) +
+  labs(title='Net Inflow Map for Louisville',subtitle='Census tract in white frame is the census tract has most inflow/outflow') +
+  mapTheme()
+
+ggplot()+
+  geom_sf(data = st_sf(LV_net_inoutflow), aes(fill=NetInflowRate)) +
+  geom_sf(data = st_sf(most_pickups_ct), color='white', fill='transparent', size=1.2) +
+  #scale_fill_continuous(limits=c(-max_inflow, max_inflow)) +
+  scale_fill_viridis(limits=c(-max_inflowRate, max_inflowRate))+
+  #geom_sf(data = st_sf(most_dropoffs_ct), color='darkblue', fill='transparent', size=1.2) +
+  labs(title='Net Inflow Rate Mapfor Louisville',subtitle='Census tract in white frame is the census tract has most inflow/outflow') +
+  mapTheme()
+
